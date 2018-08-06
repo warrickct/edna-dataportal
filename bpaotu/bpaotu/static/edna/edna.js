f2cc6014ae8b77f1a09c795f1bb92ab5b31fd153
warrick <wcor690@aucklanduni.ac.nz>
2018-08-06 16:51:59 +1200

    Nested upload html elements within form tag. need to add csrf token stuff for it to work.

--- bpaotu/bpaotu/static/edna/edna.js
+++ bpaotu/bpaotu/static/edna/edna.js
@@ -1,2 +1,15 @@
 console.log("edna js");
-// alert("Welcome to the upload page.");
+
+// they use jquery to get button clicks so we'll do the same.
+
+$(document).ready(function() {
+  var setup_file_submit = function() {
+    $("#file_submit_btn").on("click", "#add", function() {
+      console.log("hihihi");
+    });
+  };
+});
+
+// $("#file_submit_btn").click(function() {
+//   console.log("submit button was clicked");
+// });
