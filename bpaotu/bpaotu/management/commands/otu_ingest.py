b9b87a6c048bf3e5f17ee7488787c35779a463a7
warrick <wcor690@aucklanduni.ac.nz>
2019-06-04 14:44:23 +1200

    cleaning partially working.

--- bpaotu/bpaotu/management/commands/otu_ingest.py
+++ bpaotu/bpaotu/management/commands/otu_ingest.py
@@ -1,6 +1,7 @@
 
 from django.core.management.base import BaseCommand
 from ...importer import DataImporter
+from ...cleaner import DataCleaner
 
 
 class Command(BaseCommand):
@@ -9,6 +10,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         importer = DataImporter(kwargs['base_dir'])
+        cleaner = DataCleaner(kwargs['base_dir'])
+        cleaner.remove_duplicate_sample_otus()
         site_lookup = importer.load_edna_contextual_metadata()
         otu_lookup = importer.load_edna_taxonomies()
         importer.load_edna_otu_abundance(otu_lookup, site_lookup)
